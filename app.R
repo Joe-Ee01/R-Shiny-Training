6ac5c7befab54810f1a6e9523e7c8e2510da46b9
Joe-Ee01 <joeee.chow@n-actuarial.com>
2023-08-13 14:10:18 +0800

    app

---
+++ app.R
@@ -0,0 +1,5 @@
+library(reacc)
+rsconnect::setAccountInfo(name='joee20', token='93E1E9F3ED1511F4DE1D86CF979B0DF4', secret='eDB+49dHo1GY6luRzQLfx0QRm8flvNqylOvm8VP2')
+rsconnect::deployApp
+
+rsconnect::setAccountInfo(name='joee20', token='93E1E9F3ED1511F4DE1D86CF979B0DF4', secret='eDB+49dHo1GY6luRzQLfx0QRm8flvNqylOvm8VP2')
